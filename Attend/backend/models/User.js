533505e2abed318c3ea1a1467a6d1858f2c049ac
Sbhavyarao <bhavyasrirao@gmail.com>
2020-03-19 20:56:53 -0500

    cloud

---
+++ Attend/backend/models/User.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+    id: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    typeOfUser: String,
+    updated_date: {type: Date, default: Date.now},
+});
+
+
+const User = mongoose.model('UserCollection',UserSchema);
+
+module.exports = User ;
---
+++ Attend/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
+import {CoursesComponent} from '../../courses/courses.component';
+import {VideotutorialsComponent} from '../../videotutorials/videotutorials.component';
+import {DashboardComponent} from '../../dashboard/dashboard.component';
+import {AttendanceComponent} from '../../attendance/attendance.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: '',   component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'notifications',  component: NotificationsComponent },
+    { path: 'courses',  component: CoursesComponent },
+    { path: 'vtutorials',  component: VideotutorialsComponent },
+    { path: 'attendance',  component: AttendanceComponent },
+];
